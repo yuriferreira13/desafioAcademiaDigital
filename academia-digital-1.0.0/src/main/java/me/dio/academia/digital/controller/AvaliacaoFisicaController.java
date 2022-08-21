9de06729697417a7c4dda34732e74ef9b17c9fef
Yuri Ferreira <ynogueiira@gmail.com>
2022-08-21 14:38:25 -0300

    crud de Avaliacões físicas
    implementacao do repository
    implementacao do service
    implementacao do dto para receber as informacoes
    implementacao do controller

--- academia-digital-1.0.0/src/main/java/me/dio/academia/digital/controller/AvaliacaoFisicaController.java
+++ academia-digital-1.0.0/src/main/java/me/dio/academia/digital/controller/AvaliacaoFisicaController.java
@@ -1,9 +1,33 @@
 package me.dio.academia.digital.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import me.dio.academia.digital.entity.AvaliacaoFisica;
+import me.dio.academia.digital.entity.form.AvaliacaoFisicaForm;
+import me.dio.academia.digital.service.impl.AvaliacaoFisicaServiceImpl;
 
 @RestController
 @RequestMapping("/avaliacoes")
 public class AvaliacaoFisicaController {
+	
+	@Autowired
+	private AvaliacaoFisicaServiceImpl service;
+	
+	@GetMapping
+	public List<AvaliacaoFisica> getAll() {
+		return service.getAll();
+	}
+	
+	@PostMapping
+	public AvaliacaoFisica create(@RequestBody AvaliacaoFisicaForm form) {
+		return service.create(form);
+	}
+	
 }
